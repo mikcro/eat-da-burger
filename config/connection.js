6be83d4789486253ffef758f637c205730808309
mikcro <nowitsforever@gmail.com>
2017-11-08 17:41:39 -0600

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,17 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
-};
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+connection = mysql.createConnection({
+    port: 3306,
+    host: process.env.db_host || "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: process.env.db_user || "f4j5lvanq2wswanv",
+    password: process.env.db_pw || "p2x0aa28zettr4yv",
+    database: process.env.db || "rp0txifvwqzbnxhr"
+});
+// };
 
 // Make connection
 connection.connect(function(err) {
